f45597586791972295d8fe43361b67a8aa9bbe43
Ayesha Zaheer <ayeshazaheer899@gmail.com>
2026-08-08 02:15:42 -0700

    Refactor formatting in Solution.cpp
    Refactored the formatting of the twoSum function for consistency.

--- LeetCode/Two-Sum/Solution.cpp
+++ LeetCode/Two-Sum/Solution.cpp
@@ -1,18 +1,18 @@
-1class Solution {
-2public:
-3    vector<int> twoSum(vector<int>& nums, int target) {
-4 unordered_map<int,int> mpp;
-5 vector<int>ans;
-6 int n=nums.size();
-7    for(int i = 0; i < n; i++) {
-8        int value = target - nums[i];
-9        if(mpp.find(value) != mpp.end()) {
-10            ans.push_back(i);
-11            ans.push_back(mpp[value]);
-12            return ans;
-13        }
-14        mpp[nums[i]] = i;
-15    }
-16    return {-1, -1};
-17    }
-18};
+class Solution {
+public:
+   vector<int> twoSum(vector<int>& nums, int target) {
+ unordered_map<int,int> mpp;
+ vector<int>ans;
+ int n=nums.size();
+  for(int i = 0; i < n; i++) {
+       int value = target - nums[i];
+       if(mpp.find(value) != mpp.end()) {
+           ans.push_back(i);
+           ans.push_back(mpp[value]);
+          return ans;
+       }
+        mpp[nums[i]] = i;
+    }
+   return {-1, -1};
+   }
+};
