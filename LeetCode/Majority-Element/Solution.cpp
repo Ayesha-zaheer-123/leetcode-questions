5215acb84a77eca03f2918ebcf8839abd6292a86
Ayesha Zaheer <ayeshazaheer899@gmail.com>
2026-08-09 13:01:36 -0700

    Update Solution.cpp

--- LeetCode/Majority-Element/Solution.cpp
+++ LeetCode/Majority-Element/Solution.cpp
@@ -1,19 +1,19 @@
-1class Solution {
-2public:
-3    int majorityElement(vector<int>& nums) {
-4        int n=nums.size();
-5        unordered_map<int,int>mpp;
-6        for(int i=0;i<n;i++) {
-7            mpp[nums[i]]++;
-8        }
-9        int maxi=0;
-10        int ans=0;
-11        for(auto it:mpp) {
-12if(it.second>maxi) {
-13maxi=it.second;
-14ans=it.first;
-15}
-16        }
-17        return ans;
-18    }
-19};
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int,int>mpp;
+        for(int i=0;i<n;i++) {
+            mpp[nums[i]]++;
+        }
+        int maxi=0;
+        int ans=0;
+        for(auto it:mpp) {
+if(it.second>maxi) {
+maxi=it.second;
+ans=it.first;
+}
+        }
+        return ans;
+    }
+};
