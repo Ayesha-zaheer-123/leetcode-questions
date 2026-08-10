cde34a91743b22362c86e27e6ea378059c18f385
Ayesha Zaheer <ayeshazaheer899@gmail.com>
2026-08-10 12:30:46 -0700

    Fix formatting and remove unnecessary lines

--- LeetCode/Majority-Element/Solution.cpp
+++ LeetCode/Majority-Element/Solution.cpp
@@ -1,21 +1,21 @@
-1class Solution {
-2public:
-3    int majorityElement(vector<int>& nums) {
-4        int n=nums.size();
-5        int ans=0;
-6        int count=0;
-7        int maxcount=0;
-8        int el=0;
-9for(int i=0;i<n;i++) {
-10    if(el==nums[i]) {
-11        count++;
-12    }else if(count==0) {
-13        el=nums[i];
-14        count=1;
-15    }else{
-16        count--;
-17    }
-18}
-19return el;
-20    }
-21};
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        int ans=0;
+        int count=0;
+        int maxcount=0;
+        int el=0;
+for(int i=0;i<n;i++) {
+    if(el==nums[i]) {
+        count++;
+    }else if(count==0) {
+        el=nums[i];
+        count=1;
+    }else{
+        count--;
+    }
+}
+return el;
+    }
+};
