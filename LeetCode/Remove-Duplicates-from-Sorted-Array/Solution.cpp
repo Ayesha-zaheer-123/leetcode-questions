65957a9fd5b477963f1a7fb29db2af508d5bca64
Ayesha Zaheer <ayeshazaheer899@gmail.com>
2026-08-12 10:23:39 -0700

    Fix formatting issues in Solution.cpp

--- LeetCode/Remove-Duplicates-from-Sorted-Array/Solution.cpp
+++ LeetCode/Remove-Duplicates-from-Sorted-Array/Solution.cpp
@@ -1,15 +1,15 @@
-1class Solution {
-2public:
-3    int removeDuplicates(vector<int>& nums) {
-4        int n = nums.size();
-5        if (n == 0) return 0;
-6        int i = 0; 
-7        for (int j = 1; j < n; j++) {
-8            if (nums[j] != nums[i]) {
-9                i++;                 
-10                nums[i] = nums[j];    
-11        }
-12        }
-13        return i + 1; 
-14    }
-15};
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) return 0;
+        int i = 0; 
+        for (int j = 1; j < n; j++) {
+            if (nums[j] != nums[i]) {
+                i++;                 
+                nums[i] = nums[j];    
+        }
+        }
+        return i + 1; 
+    }
+};
