72e2b21a31a30e47f20f1410b0c2471c0911f6b5
Ayesha Zaheer <ayeshazaheer899@gmail.com>
2026-08-13 12:44:47 -0700

    Fix formatting issues in Solution.cpp

--- LeetCode/Rearrange-Array-Elements-by-Sign/Solution.cpp
+++ LeetCode/Rearrange-Array-Elements-by-Sign/Solution.cpp
@@ -1,19 +1,19 @@
-1class Solution {
-2public:
-3    vector<int> rearrangeArray(vector<int>& nums) {
-4        int n=nums.size();
-5       vector<int>ans(n,0);
-6       int pos=0;
-7       int neg=1;
-8        for(int i=0;i<n;i++) {
-9            if(nums[i]<0) {
-10                ans[neg]=nums[i];
-11                neg+=2;
-12            }else{
-13                ans[pos]=nums[i];
-14                pos+=2;
-15            }
-16        }
-17       return ans;
-18    }
-19};
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n=nums.size();
+       vector<int>ans(n,0);
+       int pos=0;
+       int neg=1;
+        for(int i=0;i<n;i++) {
+            if(nums[i]<0) {
+                ans[neg]=nums[i];
+                neg+=2;
+            }else{
+                ans[pos]=nums[i];
+                pos+=2;
+            }
+        }
+       return ans;
+    }
+};
