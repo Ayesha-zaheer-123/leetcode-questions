1dcf0dd6e4e88bd7f0e854735345a5e7480fcd73
Ayesha Zaheer <ayeshazaheer899@gmail.com>
2026-08-13 12:25:13 -0700

    Add solution for Rearrange Array Elements by Sign

--- LeetCode/Rearrange-Array-Elements-by-Sign/Solution.cpp
+++ LeetCode/Rearrange-Array-Elements-by-Sign/Solution.cpp
@@ -1,39 +1,19 @@
-class Solution {
-public:
-    vector<int> rearrangeArray(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>pos;
-        vector<int>neg;
-        for(int i=0;i<n;i++) {
-            if(nums[i]>=0) {
-                pos.push_back(nums[i]);
-            }else{
-                neg.push_back(nums[i]);
-            }
-        }
-        int n1=pos.size();
-    int n2=neg.size();
-    int i=0;
-    int j=0;
-    int k=0;
-    while(i<n1&&j<n2) {
-        nums[k]=pos[i];
-        i++;
-        k++;
-        nums[k]=neg[j];
-        j++;
-        k++;
-    }
-    while(i<n1) {
-        nums[k]=pos[i];
-        i++;
-        k++;
-    }
-     while(j<n2) {
-    nums[k]=neg[j];
-        j++;
-        k++;
-    }
-    return nums;
-    }
-};
+1class Solution {
+2public:
+3    vector<int> rearrangeArray(vector<int>& nums) {
+4        int n=nums.size();
+5       vector<int>ans(n,0);
+6       int pos=0;
+7       int neg=1;
+8        for(int i=0;i<n;i++) {
+9            if(nums[i]<0) {
+10                ans[neg]=nums[i];
+11                neg+=2;
+12            }else{
+13                ans[pos]=nums[i];
+14                pos+=2;
+15            }
+16        }
+17       return ans;
+18    }
+19};
