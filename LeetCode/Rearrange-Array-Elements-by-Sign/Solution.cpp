7fd331c7a5003e16a48539f057bc070a874aefc4
Ayesha Zaheer <ayeshazaheer899@gmail.com>
2026-08-13 12:11:36 -0700

    Add solution for Rearrange Array Elements by Sign

--- LeetCode/Rearrange-Array-Elements-by-Sign/Solution.cpp
+++ LeetCode/Rearrange-Array-Elements-by-Sign/Solution.cpp
@@ -4,32 +4,36 @@
 4        int n=nums.size();
 5        vector<int>pos;
 6        vector<int>neg;
-7        vector<int>ans;
-8        for(int i=0;i<n;i++) {
-9            if(nums[i]>=0) {
-10                pos.push_back(nums[i]);
-11            }else{
-12                neg.push_back(nums[i]);
-13            }
-14        }
-15        int n1=pos.size();
-16    int n2=neg.size();
-17    int i=0;
-18    int j=0;
+7        for(int i=0;i<n;i++) {
+8            if(nums[i]>=0) {
+9                pos.push_back(nums[i]);
+10            }else{
+11                neg.push_back(nums[i]);
+12            }
+13        }
+14        int n1=pos.size();
+15    int n2=neg.size();
+16    int i=0;
+17    int j=0;
+18    int k=0;
 19    while(i<n1&&j<n2) {
-20        ans.push_back(pos[i]);
+20        nums[k]=pos[i];
 21        i++;
-22        ans.push_back(neg[j]);
-23        j++;
-24    }
-25    while(i<n1) {
-26        ans.push_back(pos[i]);
-27        i++;
-28    }
-29     while(j<n2) {
-30        ans.push_back(neg[j]);
-31        j++;
-32    }
-33    return ans;
-34    }
-35};
+22        k++;
+23        nums[k]=neg[j];
+24        j++;
+25        k++;
+26    }
+27    while(i<n1) {
+28        nums[k]=pos[i];
+29        i++;
+30        k++;
+31    }
+32     while(j<n2) {
+33    nums[k]=neg[j];
+34        j++;
+35        k++;
+36    }
+37    return nums;
+38    }
+39};
