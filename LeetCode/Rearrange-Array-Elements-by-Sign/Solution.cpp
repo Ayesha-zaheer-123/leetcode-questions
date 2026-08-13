cc85be9a3036dc770747bd35e6c960474a6b04d1
Ayesha Zaheer <ayeshazaheer899@gmail.com>
2026-08-13 12:13:29 -0700

    Update Solution.cpp

--- LeetCode/Rearrange-Array-Elements-by-Sign/Solution.cpp
+++ LeetCode/Rearrange-Array-Elements-by-Sign/Solution.cpp
@@ -1,39 +1,39 @@
-1class Solution {
-2public:
-3    vector<int> rearrangeArray(vector<int>& nums) {
-4        int n=nums.size();
-5        vector<int>pos;
-6        vector<int>neg;
-7        for(int i=0;i<n;i++) {
-8            if(nums[i]>=0) {
-9                pos.push_back(nums[i]);
-10            }else{
-11                neg.push_back(nums[i]);
-12            }
-13        }
-14        int n1=pos.size();
-15    int n2=neg.size();
-16    int i=0;
-17    int j=0;
-18    int k=0;
-19    while(i<n1&&j<n2) {
-20        nums[k]=pos[i];
-21        i++;
-22        k++;
-23        nums[k]=neg[j];
-24        j++;
-25        k++;
-26    }
-27    while(i<n1) {
-28        nums[k]=pos[i];
-29        i++;
-30        k++;
-31    }
-32     while(j<n2) {
-33    nums[k]=neg[j];
-34        j++;
-35        k++;
-36    }
-37    return nums;
-38    }
-39};
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>pos;
+        vector<int>neg;
+        for(int i=0;i<n;i++) {
+            if(nums[i]>=0) {
+                pos.push_back(nums[i]);
+            }else{
+                neg.push_back(nums[i]);
+            }
+        }
+        int n1=pos.size();
+    int n2=neg.size();
+    int i=0;
+    int j=0;
+    int k=0;
+    while(i<n1&&j<n2) {
+        nums[k]=pos[i];
+        i++;
+        k++;
+        nums[k]=neg[j];
+        j++;
+        k++;
+    }
+    while(i<n1) {
+        nums[k]=pos[i];
+        i++;
+        k++;
+    }
+     while(j<n2) {
+    nums[k]=neg[j];
+        j++;
+        k++;
+    }
+    return nums;
+    }
+};
