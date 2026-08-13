eb2f10b54063efc5f9e253769a872ceb59207cee
Ayesha Zaheer <ayeshazaheer899@gmail.com>
2026-08-13 12:42:36 -0700

    Refactor maxProfit to use vector instead of array

--- LeetCode/Best-Time-to-Buy-and-Sell-Stock/Solution.cpp
+++ LeetCode/Best-Time-to-Buy-and-Sell-Stock/Solution.cpp
@@ -1,15 +1,16 @@
 class Solution {
-    public:
-      int maxProfit(int[] prices) {
-        int buy = prices[0];
-        int profit = 0;
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] < buy) {
-                buy = prices[i];
-            } else if (prices[i] - buy > profit) {
-                profit = prices[i] - buy;
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+      int buy=prices[0];
+      int profit=0;
+        for(int j=1;j<n;j++) {
+            if(prices[j]<buy) {
+    buy=prices[j];
+            }else if(prices[j]-buy>profit){
+    profit=prices[j]-buy;
             }
         }
-        return profit;
+return profit;
     }
-}
+};
