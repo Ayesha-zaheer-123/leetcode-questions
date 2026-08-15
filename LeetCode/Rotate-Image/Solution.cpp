28342898eb5bd5719f8b295a34404ae7e3a9bf1f
Ayesha Zaheer <ayeshazaheer899@gmail.com>
2026-08-15 11:59:12 -0700

    Fix formatting and indentation in rotate function

--- LeetCode/Rotate-Image/Solution.cpp
+++ LeetCode/Rotate-Image/Solution.cpp
@@ -1,15 +1,15 @@
-1class Solution {
-2public:
-3    void rotate(vector<vector<int>>& matrix) {
-4        int rows=matrix.size();
-5        int cols=matrix[0].size();
-6        for(int i=0;i<rows;i++) {
-7            for(int j=i+1;j<cols;j++) {
-8              swap(matrix[i][j],matrix[j][i]);
-9            }
-10        }
-11for(int i=0;i<rows;i++) {
-12    reverse(matrix[i].begin(),matrix[i].end());
-13}
-14    }
-15};
+class Solution {
+public:
+   void rotate(vector<vector<int>>& matrix) {
+       int rows=matrix.size();
+       int cols=matrix[0].size();
+       for(int i=0;i<rows;i++) {
+           for(int j=i+1;j<cols;j++) {
+             swap(matrix[i][j],matrix[j][i]);
+           }
+       }
+for(int i=0;i<rows;i++) {
+    reverse(matrix[i].begin(),matrix[i].end());
+}
+   }
+};
