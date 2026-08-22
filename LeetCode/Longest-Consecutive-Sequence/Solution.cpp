972ab324c226d3a387ddd2cc3024a2f8533fdd42
Ayesha Zaheer <ayeshazaheer899@gmail.com>
2026-08-22 11:04:25 -0700

    Implement longest consecutive sequence function

--- LeetCode/Longest-Consecutive-Sequence/Solution.cpp
+++ LeetCode/Longest-Consecutive-Sequence/Solution.cpp
@@ -1,1 +1,24 @@
-[1, 2, 3, 4]
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        if(n==0) {
+            return 0;
+        }
+        int smallest=INT_MIN;
+        int count=0;
+        int largest=1;
+        for(int i=0;i<n;i++) {
+            if(nums[i]-1==smallest) {
+                count++;
+                smallest=nums[i];
+            }else if(nums[i]!=smallest) {
+                count=1;
+                smallest=nums[i];
+            }
+            largest=max(largest,count);
+        }
+        return largest;
+    }
+};
