a4d61e2d0f2a8554babe8e6b3fde826dadd29cae
Ayesha Zaheer <ayeshazaheer899@gmail.com>
2026-08-24 10:58:02 -0700

    Refactor merge function in Solution.cpp

--- LeetCode/Merge-Sorted-Array/Solution.cpp
+++ LeetCode/Merge-Sorted-Array/Solution.cpp
@@ -1,23 +1,23 @@
-1class Solution {
-2public:
-3    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-4        int i=m-1;
-5        int j=n-1;
-6        int k=m+n-1;
-7        while(i>=0&&j>=0) {
-8            if(nums1[i]>=nums2[j]) {
-9                nums1[k]=nums1[i];
-10                i--;
-11            }else{
-12                nums1[k]=nums2[j];
-13                j--;
-14            }
-15            k--;
-16        }
-17        while(j>=0) {
-18            nums1[k]=nums2[j];
-19            j--;
-20            k--;
-21        }
-22    }
-23};
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+       int i=m-1;
+       int j=n-1;
+       int k=m+n-1;
+      while(i>=0&&j>=0) {
+          if(nums1[i]>=nums2[j]) {
+               nums1[k]=nums1[i];
+               i--;
+          }else{
+              nums1[k]=nums2[j];
+              j--;
+          }
+          k--;
+      }
+      while(j>=0) {
+          nums1[k]=nums2[j];
+          j--;
+          k--;
+      }
+   }
+};
