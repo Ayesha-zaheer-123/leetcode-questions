1e28b25ee64b4b2849970699154c49eb6f728452
Ayesha Zaheer <ayeshazaheer899@gmail.com>
2026-08-24 11:00:42 -0700

    Fix class definition and formatting in Solution.cpp

--- LeetCode/Majority-Element-II/Solution.cpp
+++ LeetCode/Majority-Element-II/Solution.cpp
@@ -1,43 +1,44 @@
-1class Solution {
-2public:
-3    vector<int> majorityElement(vector<int>& nums) {
-4        vector<int>ans;
-5        int n=nums.size();
-6        if(n==1) {
-7            ans.push_back(nums[0]);
-8            return ans;
-9        }
-10    int count1=0;
-11    int count2=0;
-12    int el1=INT_MIN;
-13    int el2=INT_MIN;
-14    for(int i=0;i<n;i++) {
-15        if(count1==0&&el2!=nums[i]) {
-16            count1=1;
-17            el1=nums[i];
-18        }else if(count2==0&&el1!=nums[i]) {
-19            count2=1;
-20            el2=nums[i];
-21        } else if(el1==nums[i]) {
-22            count1++;
-23        } else if(el2==nums[i]) {
-24            count2++;
-25        }else{
-26            count1--;
-27            count2--;
-28        }
-29    }
-30    count1=0;
-31    count2=0;
-32    for(int i=0;i<n;i++) {
-33        if(nums[i]==el1) {
-34            count1++;
-35        }else if(nums[i]==el2) {
-36            count2++;
-37        }
-38    }
-39if(count1 > n/3) ans.push_back(el1);
-40if(count2 > n/3) ans.push_back(el2);
-41        return ans;
-42            }
-43};
+
+Class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        vector<int>ans;
+        int n=nums.size();
+        if(n==1) {
+            ans.push_back(nums[0]);
+            return ans;
+        }
+    int count1=0;
+    int count2=0;
+    int el1=INT_MIN;
+    int el2=INT_MIN;
+    for(int i=0;i<n;i++) {
+        if(count1==0&&el2!=nums[i]) {
+            count1=1;
+            el1=nums[i];
+        }else if(count2==0&&el1!=nums[i]) {
+            count2=1;
+            el2=nums[i];
+        } else if(el1==nums[i]) {
+            count1++;
+        } else if(el2==nums[i]) {
+            count2++;
+        }else{
+            count1--;
+            count2--;
+        }
+    }
+    count1=0;
+    count2=0;
+    for(int i=0;i<n;i++) {
+        if(nums[i]==el1) {
+            count1++;
+        }else if(nums[i]==el2) {
+            count2++;
+        }
+    }
+if(count1 > n/3) ans.push_back(el1);
+if(count2 > n/3) ans.push_back(el2);
+        return ans;
+            }
+};
