4f5c05529a74c9fa219620ad26630c05b649b09d
Ayesha Zaheer <ayeshazaheer899@gmail.com>
2026-08-25 13:06:25 -0700

    Refactor 3Sum solution for clarity and efficiency

--- LeetCode/3Sum/Solution.cpp
+++ LeetCode/3Sum/Solution.cpp
@@ -1,31 +1,31 @@
-1class Solution {
-2public:
-3    vector<vector<int>> threeSum(vector<int>& nums) {
-4        vector<vector<int>>finalans;
-5        int n=nums.size();
-6        sort(nums.begin(),nums.end());
-7       for(int i=0;i<n;i++) {
-8         if(i>0&&nums[i]==nums[i-1]) continue;
-9        int j=i+1;
-10        int k=n-1;
-11        while(j<k) {
-12            if(nums[i]+nums[j]+nums[k]==0) {
-13                finalans.push_back({nums[i],nums[j],nums[k]});
-14                j++;
-15                k--;
-16                while(j<k&&nums[j]==nums[j-1]) {
-17        j++;
-18            }
-19            while(j<k&&nums[k]==nums[k+1]) {
-20        k--;
-21            }
-22            }else if(nums[i]+nums[j]+nums[k]>0) {
-23                k--;
-24            }else{
-25                j++;
-26            }
-27        }
-28       }
-29       return finalans;
-30    }
-31};
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>>finalans;
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+       for(int i=0;i<n;i++) {
+         if(i>0&&nums[i]==nums[i-1]) continue;
+        int j=i+1;
+        int k=n-1;
+        while(j<k) {
+            if(nums[i]+nums[j]+nums[k]==0) {
+                finalans.push_back({nums[i],nums[j],nums[k]});
+                j++;
+                k--;
+                while(j<k&&nums[j]==nums[j-1]) {
+        j++;
+            }
+            while(j<k&&nums[k]==nums[k+1]) {
+        k--;
+            }
+            }else if(nums[i]+nums[j]+nums[k]>0) {
+                k--;
+            }else{
+                j++;
+            }
+        }
+       }
+       return finalans;
+    }
+};
