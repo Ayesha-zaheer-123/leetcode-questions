625f4abb55b8d7543747638b002a59bef46a4393
Ayesha Zaheer <ayeshazaheer899@gmail.com>
2026-08-29 13:37:28 -0700

    Add solution for Maximum Product Subarray

--- LeetCode/Maximum-Product-Subarray/Solution.cpp
+++ LeetCode/Maximum-Product-Subarray/Solution.cpp
@@ -3,31 +3,25 @@
 3    int maxProduct(vector<int>& nums) {
 4         int positive = nums[0];
 5    int negative = nums[0];
-6
-7    int answer = nums[0];
-8
-9    for(int i = 1; i < nums.size(); i++) {
-10
-11        int x = nums[i];
-12
-13        int newPositive = max({
-14            x,
-15            x * positive,
-16            x * negative
-17        });
-18
-19        int newNegative = min({
-20            x,
-21            x * positive,
-22            x * negative
-23        });
+6    int answer = nums[0];
+7    for(int i = 1; i < nums.size(); i++) {
+8        int x = nums[i];
+9        int newPositive = max({
+10            x,
+11            x * positive,
+12            x * negative
+13        });
+14        int newNegative = min({
+15            x,
+16            x * positive,
+17            x * negative
+18        });
+19
+20        positive = newPositive;
+21        negative = newNegative;
+22        answer = max(answer, positive);
+23    }
 24
-25        positive = newPositive;
-26        negative = newNegative;
-27
-28        answer = max(answer, positive);
-29    }
-30
-31    return answer;
-32    }
-33};
+25    return answer;
+26    }
+27};
