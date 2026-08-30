a12f21f51fdde065d881e17e412000ce2c840c40
Ayesha Zaheer <ayeshazaheer899@gmail.com>
2026-08-30 10:39:47 -0700

    Fix formatting and remove unnecessary characters

--- LeetCode/Maximum-Product-Subarray/Solution.cpp
+++ LeetCode/Maximum-Product-Subarray/Solution.cpp
@@ -1,22 +1,22 @@
-1class Solution {
-2public:
-3    int maxProduct(vector<int>& nums) {
-4         int positive = nums[0];
-5    int negative = nums[0];
-6    int answer = nums[0];
-7    for(int i = 1; i < nums.size(); i++) {
-8        int x = nums[i];
-9        int newpositive = max({
-10            x, x * positive,x * negative
-11        });
-12        int newnegative = min({
-13            x, x * positive,x * negative
-14        });
-15        positive = newpositive;
-16        negative = newnegative;
-17        answer = max(answer, positive);
-18    }
-19
-20    return answer;
-21    }
-22};
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+         int positive = nums[0];
+    int negative = nums[0];
+    int answer = nums[0];
+    for(int i = 1; i < nums.size(); i++) {
+        int x = nums[i];
+        int newpositive = max({
+            x, x * positive,x * negative
+        });
+        int newnegative = min({
+            x, x * positive,x * negative
+        });
+        positive = newpositive;
+        negative = newnegative;
+        answer = max(answer, positive);
+    }
+
+    return answer;
+    }
+};
