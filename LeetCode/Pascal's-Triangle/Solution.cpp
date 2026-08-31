c77773cef0b70a007231ee666ca9e7af1249f7b1
Ayesha Zaheer <ayeshazaheer899@gmail.com>
2026-08-31 12:08:01 -0700

    Fix formatting and indentation in Solution.cpp

--- LeetCode/Pascal's-Triangle/Solution.cpp
+++ LeetCode/Pascal's-Triangle/Solution.cpp
@@ -1,19 +1,19 @@
-1class Solution {
-2public:
-3    vector<vector<int>> generate(int numRows) {
-4        vector<vector<int>>result;
-5        for(int i=0;i<numRows;i++) {
-6            result.push_back({});
-7for(int j=0;j<=i;j++) {
-8    result[i].push_back(1);
-9}
-10if(i>1) {
-11    for(int j=1;j<i;j++) {
-12        result[i][j]=result[i-1][j]+result[i-1][j-1];
-13    }
-14}
-15    
-16    }
-17    return result;
-18    }
-19};
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+       vector<vector<int>>result;
+        for(int i=0;i<numRows;i++) {
+           result.push_back({});
+for(int j=0;j<=i;j++) {
+    result[i].push_back(1);
+}
+if(i>1) {
+   for(int j=1;j<i;j++) {
+       result[i][j]=result[i-1][j]+result[i-1][j-1];
+   }
+}
+   
+  }
+  return result;
+   }
+};
