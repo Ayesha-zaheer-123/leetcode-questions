c258a4d6f128d3416d606bcf2d7de19622108395
Ayesha Zaheer <ayeshazaheer899@gmail.com>
2026-09-01 07:56:14 -0700

    Fix formatting and indentation in Solution.cpp

--- LeetCode/Binary-Search/Solution.cpp
+++ LeetCode/Binary-Search/Solution.cpp
@@ -1,19 +1,19 @@
-1class Solution {
-2public:
-3    int search(vector<int>& nums, int target) {
-4     int n=nums.size();
-5        int low=0;
-6        int high=n-1;
-7        while(low<=high) {
-8            int mid=(low+high)/2;
-9            if(nums[mid]==target) {
-10                return mid;
-11            }else if(nums[mid]>target) {
-12        high=mid-1;
-13            }else{
-14                low=mid+1;
-15            }
-16        }
-17        return -1;
-18    }
-19};
+class Solution {
+public:
+   int search(vector<int>& nums, int target) {
+    int n=nums.size();
+    int low=0;
+      int high=n-1;
+      while(low<=high) {
+           int mid=(low+high)/2;
+          if(nums[mid]==target) {
+               return mid;
+          }else if(nums[mid]>target) {
+       high=mid-1;
+           }else{
+                low=mid+1;
+          }
+      }
+        return -1;
+   }
+};
