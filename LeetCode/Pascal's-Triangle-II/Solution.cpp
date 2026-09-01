78f1f6a3bc2a8a8a59648d41721bde2b5e34dbea
Ayesha Zaheer <ayeshazaheer899@gmail.com>
2026-09-01 07:53:19 -0700

    Refactor Solution class for Pascal's Triangle II

--- LeetCode/Pascal's-Triangle-II/Solution.cpp
+++ LeetCode/Pascal's-Triangle-II/Solution.cpp
@@ -1,19 +1,18 @@
-1class Solution {
-2public:
-3    vector<int> getRow(int rowIndex) {
-4         vector<vector<int>> triangle;
-5    for (int i = 0; i <= rowIndex; i++) {
-6        triangle.push_back({});
-7       for(int j=0;j<=i;j++) {
-8        triangle[i].push_back(1);
-9       }
-10       if(i>1) {
-11        for (int j = 1; j < i; j++) {
-12         triangle[i][j]= triangle[i - 1][j - 1] + triangle[i - 1][j];
-13        }
-14    }
-15    }
-16
-17    return triangle[rowIndex];
-18    }
-19};
+class Solution {
+public:
+  vector<int> getRow(int rowIndex) {
+        vector<vector<int>> triangle;
+    for (int i = 0; i <= rowIndex; i++) {
+       triangle.push_back({});
+     for(int j=0;j<=i;j++) {
+      triangle[i].push_back(1);
+     }
+      if(i>1) {
+      for (int j = 1; j < i; j++) {
+       triangle[i][j]= triangle[i - 1][j - 1] + triangle[i - 1][j];
+      }
+   }    }
+
+    return triangle[rowIndex];
+   }
+};
