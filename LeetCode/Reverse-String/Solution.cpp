ab096fa4c558087462f769fe32d71e73c2a398bf
Ayesha Zaheer <ayeshazaheer899@gmail.com>
2026-09-02 08:49:55 -0700

    Add solution for Reverse String

--- LeetCode/Reverse-String/Solution.cpp
+++ LeetCode/Reverse-String/Solution.cpp
@@ -1,6 +1,13 @@
 1class Solution {
 2public:
 3    void reverseString(vector<char>& s) {
-4        reverse(s.begin(),s.end());
-5    }
-6};
+4       int n=s.size();
+5       int end=n-1;
+6      int start=0;
+7       while(start<=end) {
+8    swap(s[start],s[end]);
+9    start++;
+10    end--;
+11       }
+12    }
+13};
