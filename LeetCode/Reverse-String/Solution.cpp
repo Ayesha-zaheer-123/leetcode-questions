1dfad5a8427809855b828980f16cf9afa6108984
Ayesha Zaheer <ayeshazaheer899@gmail.com>
2026-09-03 07:24:54 -0700

    Fix formatting and indentation in reverseString method

--- LeetCode/Reverse-String/Solution.cpp
+++ LeetCode/Reverse-String/Solution.cpp
@@ -1,13 +1,13 @@
-1class Solution {
-2public:
-3    void reverseString(vector<char>& s) {
-4       int n=s.size();
-5       int end=n-1;
-6      int start=0;
-7       while(start<=end) {
-8    swap(s[start],s[end]);
-9    start++;
-10    end--;
-11       }
-12    }
-13};
+class Solution {
+public:
+   void reverseString(vector<char>& s) {
+     int n=s.size();
+     int end=n-1;
+     int start=0;
+       while(start<=end) {
+   swap(s[start],s[end]);
+   start++;
+   end--;
+       }
+    }
+};
