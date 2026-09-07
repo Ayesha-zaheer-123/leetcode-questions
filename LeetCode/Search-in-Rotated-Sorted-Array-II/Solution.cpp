7e75b354fac0afa88905ec4dcc601b1b2aaa29ed
Ayesha Zaheer <ayeshazaheer899@gmail.com>
2026-09-07 04:55:00 -0700

    Implement binary search in rotated sorted array
    Refactor search method to implement binary search in a rotated sorted array.

--- LeetCode/Search-in-Rotated-Sorted-Array-II/Solution.cpp
+++ LeetCode/Search-in-Rotated-Sorted-Array-II/Solution.cpp
@@ -1,6 +1,31 @@
-    for(int i = 0; i<nums.size(); i++)
-    {
-        if(nums[i] == target)
-            return 1;
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+    int low=0;
+    int high=nums.size()-1;
+    while(low<=high) {
+        int mid=(low+high)/2;
+        if(nums[mid]==target)  return true;
+        if(nums[low]==nums[mid]&&nums[mid]==nums[high]) {
+            high--;
+            low++;
+            continue;
+
+        }
+        else if(nums[low]<=nums[mid]) {
+            if(nums[low]<=target&&target<=nums[mid]) {
+               high=mid-1;
+            }else{
+              low=mid+1;
+            }
+        }else{
+if(target>nums[mid]&&target<=nums[high]) {
+    low=mid+1;
+}else{
+    high=mid-1;
+}
+        }
     }
-    return 0;
+    return  false;
+    }
+};
