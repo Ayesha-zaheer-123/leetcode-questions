35fbee8750ad305da70fe2a165f208c84e63ad95
Ayesha Zaheer <ayeshazaheer899@gmail.com>
2026-09-08 12:55:20 -0700

    Update Solution.cpp

--- LeetCode/Single-Element-in-a-Sorted-Array/Solution.cpp
+++ LeetCode/Single-Element-in-a-Sorted-Array/Solution.cpp
@@ -1,39 +1,30 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-
-        int n = nums.size();
-
-        if (n == 1)
+        int low=0;
+        int high=nums.size()-1;
+        int n=nums.size();
+        if(n==1) return nums[0];
+          if (nums[0] != nums[1])
             return nums[0];
-
-        if (nums[0] != nums[1])
-            return nums[0];
-
-        if (nums[n - 1] != nums[n - 2])
-            return nums[n - 1];
-
-        int start = 1;
-        int end = n - 2;
-
-        while (start <= end) {
-
-            int mid = start + (end - start) / 2;
-
-            if (nums[mid] != nums[mid - 1] &&
-                nums[mid] != nums[mid + 1]) {
+        while(low<=high) {
+            int mid=(low+high)/2;
+            if(nums[mid]!=nums[mid-1]&&nums[mid]!=nums[mid+1])  {
                 return nums[mid];
-            }
-
-            if ((mid % 2 == 0 && nums[mid] == nums[mid + 1]) ||
-                (mid % 2 == 1 && nums[mid] == nums[mid - 1])) {
-                start = mid + 1;
-            }
-            else {
-                end = mid - 1;
+            }else if(mid%2==1) {
+                if(nums[mid]==nums[mid-1]) {
+                    low=mid+1;
+                }else{
+                    high=mid-1;
+                }
+            }else{
+                if(nums[mid]==nums[mid-1]) {
+                    high=mid-1;
+                }else{
+                    low=mid+1;
+                }
             }
         }
-
-        return -1;
+        return nums[low-1];
     }
 };
