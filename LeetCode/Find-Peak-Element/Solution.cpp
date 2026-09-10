be4ace2e658fb30ba07970f1e4bc3f4c8def4553
Ayesha Zaheer <ayeshazaheer899@gmail.com>
2026-09-10 11:57:01 -0700

    Refactor findPeakElement for edge case handling
    Refactor findPeakElement function to handle edge cases and improve readability.

--- LeetCode/Find-Peak-Element/Solution.cpp
+++ LeetCode/Find-Peak-Element/Solution.cpp
@@ -1,9 +1,30 @@
-int start = 0;
-int end = n-1;
-
-while(start <= end) {
-	int mid = start + (end - start)/2;
-	if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) return mid;   // if mid == peak ( case 2 )
-	else if(nums[mid] < nums[mid-1]) end = mid - 1; // downward slope and search space left side ( case 1)
-	else if(nums[mid] < nums[mid+1]) start = mid + 1; // upward slope and search space right side ( case 3 )
-}
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n=nums.size();
+        if(n==0) return -1;
+        if(n==1) return 0;
+        if(nums[n-1]>nums[n-2]) {
+            return n-1;
+        }
+        int low=0;
+        int high=n-2;
+        while(low<=high) {
+            if(low==0) {
+                if(nums[low]>nums[low+1]) {
+                    return low;
+                }
+        low++;
+            }
+            int mid=(low+high)/2;
+            if(nums[mid]>nums[mid-1]&&nums[mid]>nums[mid+1]) {
+                return mid;
+            }if(nums[mid]<nums[mid-1]) {
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return -1;
+    }
+};
