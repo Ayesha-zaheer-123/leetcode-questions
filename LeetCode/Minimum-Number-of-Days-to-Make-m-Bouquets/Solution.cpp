de096bb53929237f3c3ee5b78e99154449b44dc7
Ayesha Zaheer <ayeshazaheer899@gmail.com>
2026-09-16 11:49:18 -0700

    Update Solution.cpp

--- LeetCode/Minimum-Number-of-Days-to-Make-m-Bouquets/Solution.cpp
+++ LeetCode/Minimum-Number-of-Days-to-Make-m-Bouquets/Solution.cpp
@@ -1,44 +1,41 @@
 class Solution {
 public:
-    int minDays(vector<int>& bloomDay, int m, int k) {
-        if ((long long)m * k > bloomDay.size()) {
-            return -1;
+bool func(int mid,vector<int>&bloomDay,int m,int k) {
+ int consective=0;
+        int flower=0;
+        int n=bloomDay.size();
+for(int i=0;i<n;i++) {
+    if(bloomDay[i]<=mid) {
+        consective++;
+        if(consective==k) {
+            flower++;
+            consective=0;
         }
-
-        int low = 1, high = 1e9;
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-
-            if (canMakeBouquets(bloomDay, m, k, mid)) {
-                high = mid;
-            } else {
-                low = mid + 1;
-            }
-        }
-
-        return low;
+    }else{
+        consective=0;
     }
-
-private:
-    bool canMakeBouquets(vector<int>& bloomDay, int m, int k, int day) {
-        int total = 0;
-        for (int i = 0; i < bloomDay.size(); i++) {
-            int count = 0;
-            while (i < bloomDay.size() && count < k && bloomDay[i] <= day) {
-                count++;
-                i++;
-            }
-
-            if (count == k) {
-                total++;
-                i--;
-            }
-
-            if (total >= m) {
-                return true;
+}
+    if(flower>=m) {
+        return true;
+    }
+    return false;
+}
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        int n=bloomDay.size();
+      int maxi= *max_element(bloomDay.begin(), bloomDay.end());
+        if((long long)m*k>n)  return -1;
+        int low=1;
+        int high=maxi;
+        int ans=maxi;
+        while(low<=high) {
+            int mid=low+(high-low)/2;
+            if(func(mid,bloomDay,m,k)) {
+ans=mid;
+high=mid-1;
+            }else{
+                low=mid+1;
             }
         }
-
-        return false;
+        return ans;
     }
 };
