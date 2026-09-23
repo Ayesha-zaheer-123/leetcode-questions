b99867c56aae011672dca3a509c8a01220f6ae9e
Ayesha Zaheer <ayeshazaheer899@gmail.com>
2026-09-23 06:01:06 -0700

    Optimize findKthPositive with binary search
    Refactor findKthPositive function to use binary search for improved efficiency.

--- LeetCode/Kth-Missing-Positive-Number/Solution.cpp
+++ LeetCode/Kth-Missing-Positive-Number/Solution.cpp
@@ -1,22 +1,18 @@
-1class Solution {
-2public:
-3    int findKthPositive(vector<int>& arr, int k) {
-4        int n =arr.size();
-5        int nums=1;
-6        int i=0;
-7        while(i<n&&k>0) {
-8            if(arr[i]==nums) {
-9                i++;
-10            }else {
-11                k--;
-12            }
-13            nums++;
-14        }
-15    while(k>0) {
-16        k--;
-17nums=nums+1;
-18    }
-19        
-20        return nums-1;
-21    }
-22};
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int n =arr.size();
+      int low=0;
+      int high=n-1;
+      while(low<=high) {
+        int mid=(low+high)/2;
+        int missing=arr[mid]-(mid+1);
+        if(missing<k) {
+            low=mid+1;
+        }else {
+            high=mid-1;
+        }
+      }
+      return low+k;
+    }
+};
