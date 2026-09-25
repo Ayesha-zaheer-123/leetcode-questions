8b8f488562402128e9202a53a7276e94a6b372ff
Ayesha Zaheer <ayeshazaheer899@gmail.com>
2026-09-25 07:33:37 -0700

    Add solution for Kth Missing Positive Number

--- LeetCode/Kth-Missing-Positive-Number/README.md
+++ LeetCode/Kth-Missing-Positive-Number/README.md
@@ -1,6 +1,6 @@
 # Kth Missing Positive Number
 
-Kth Missing Positive Number  Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
+Can you solve this real interview question? Kth Missing Positive Number - Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
 
 Return the kth positive integer that is missing from this array.
 
--- LeetCode/Kth-Missing-Positive-Number/Solution.cpp
+++ LeetCode/Kth-Missing-Positive-Number/Solution.cpp
@@ -1,18 +1,17 @@
-class Solution {
-public:
-    int findKthPositive(vector<int>& arr, int k) {
-        int n =arr.size();
-      int low=0;
-      int high=n-1;
-      while(low<=high) {
-        int mid=(low+high)/2;
-        int missing=arr[mid]-(mid+1);
-        if(missing<k) {
-            low=mid+1;
-        }else {
-            high=mid-1;
-        }
-      }
-      return low+k;
-    }
-};
+1class Solution {
+2public:
+3    int findKthPositive(vector<int>& arr, int k) {
+4        int low=0;
+5     int high=arr.size()-1;
+6     while(low<=high) {
+7        int mid=(low+high)/2;
+8        int missing=arr[mid]-(mid+1);
+9        if(missing<k) {
+10            low=mid+1;
+11        }else{
+12            high=mid-1;
+13        }
+14     }
+15     return low+k;
+16    }
+17};
